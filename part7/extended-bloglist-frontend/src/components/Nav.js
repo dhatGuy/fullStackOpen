4be997d05c962187663a28b4de473b566565d0b6
Odunsi Joseph <odunsiolakunbi@gmail.com>
2021-06-16 01:40:39 +0100

    part 7

---
+++ part7/extended-bloglist-frontend/src/components/Nav.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Spacer,
+  Link,
+  Text,
+  useColorModeValue,
+  HStack,
+} from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+const Nav = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch({
+      type: "LOGOUT",
+    });
+  };
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex padding="5" justify="space-between" align="center">
+        <Box>
+          <Heading as={NavLink} to="/" size="md">
+            Blog App
+          </Heading>
+        </Box>
+        {user !== null ? (
+          <HStack spacing="24px">
+            <Spacer />
+            <Box>
+              <Link
+                as={NavLink}
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/blogs"
+              >
+                Blogs
+              </Link>
+            </Box>
+            <Box>
+              <Link
+                as={NavLink}
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/users"
+              >
+                Users
+              </Link>
+            </Box>
+            <Box>
+              <Text>{user.name} logged in</Text>
+            </Box>
+            <Box>
+              <Button
+                to="/login"
+                color="white"
+                bg={"blue.700"}
+                as={NavLink}
+                onClick={logout}
+              >
+                Log out
+              </Button>
+            </Box>
+          </HStack>
+        ) : null}
+      </Flex>
+    </Box>
+  );
+};
+
+export default Nav;
